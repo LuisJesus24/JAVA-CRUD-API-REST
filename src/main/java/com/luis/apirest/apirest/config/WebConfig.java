d760d7a356c596b9ed2397f045d4180a95d45f86
LuisJesus24 <luis.j.fcfm@gmail.com>
2025-01-09 17:04:07 -0600

    Remove SendGrid API key from application.properties

---
+++ src/main/java/com/luis/apirest/apirest/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.luis.apirest.apirest.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todos los endpoints
+                .allowedOrigins("https://67814a4de99cf3405146d23e--precious-lamington-e883a8.netlify.app") // Tu dominio
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Permitir todos los headers
+    }
+}
