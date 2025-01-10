0ea0f69e8bce08fbfa84adf448171ed42d069bf7
LuisJesus24 <luis.j.fcfm@gmail.com>
2025-01-10 11:56:32 -0600

    Correccion de clave smtp

--- src/main/java/com/luis/apirest/apirest/configuration/WebConfig.java
+++ src/main/java/com/luis/apirest/apirest/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-package com.luis.apirest.apirest.config;
+package com.luis.apirest.apirest.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todos los endpoints
-                .allowedOrigins("https://67814a4de99cf3405146d23e--precious-lamington-e883a8.netlify.app") // Tu dominio
+                .allowedOrigins("https://67814a4de99cf3405146d23e--precious-lamington-e883a8.netlify.app") // Tu dominio Netlify
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                 .allowedHeaders("*"); // Permitir todos los headers
     }
